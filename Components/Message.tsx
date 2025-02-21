9ea49cf1c81f11c0265d9ce1c8fdcc8069b67aba
BranellKaunda <branellk@gmail.com>
2025-02-21 14:16:48 +0100

    complte app heading

--- Components/Message.tsx
+++
@@ -1,9 +0,0 @@
-interface Props {
-  name: string;
-}
-
-const Message = ({ name }: Props) => {
-  return <h1>Hello {name}</h1>;
-};
-
-export default Message;
---
+++ Components/Search.jsx
@@ -0,0 +1,11 @@
+const Search = ({searchTerm, setSearchTerm}) => {
+  return <div className="search">
+    <div>
+      <img src="search.svg" alt="search" />
+
+      <input type="text" placeholder="Search through thousands of movies" onChange={(event) => setSearchTerm(event.target.value)}/>
+    </div>
+  </div>;
+};
+
+export default Search;
